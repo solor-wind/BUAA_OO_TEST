a3fa938f7fa2bdbf2bcf1826f0fc7fce8c03460b
张鑫 <22373090@buaa.edu.cn>
2024-05-28 15:17:18 +0800

    初步提交数据生成器

--- DataGenerator.py
+++ DataGenerator.py
@@ -14,16 +14,22 @@ num_person = int(config['max_num_of_person'])
 query_prob = float(config['query_prob'])
 borrow_prob = float(config['borrow_prob'])
 order_prob = float(config['order_prob'])
+donate_prob = float(config['donate_prob'])
+renew_prob = float(config['renew_prob'])
 return_prob = float(config['return_prob'])
 pick_1_prob = float(config['pick_1_prob'])
 pick_2_prob = float(config['pick_2_prob'])
 
 
-def split_list_randomly(lst, n):
-    groups = [[] for _ in range(n)]
-    random.shuffle(lst)
-    for item in lst:
-        random.choice(groups).append(item)
+def split_list_randomly(elements, m):
+    n = len(elements)
+    separator_positions = sorted(random.choices(list(range(n - 1)), k=m - 1))
+    groups = []
+    start = 0
+    for pos in separator_positions:
+        groups.append(elements[start:pos + 1])
+        start = pos + 1
+    groups.append(elements[start:])
     return groups
 
 
@@ -52,12 +58,12 @@ class data_generator:
         self.index = 0
         self.library = library
         self.commands = []
+        self.books = set()
         self.init_command_list = []  # 初始化图书信息的命令里列表
         self.date2commands = {}
         self.generate_books()
         self.generate_persons()
         self.generate_basic_commands()
-        pass
         self.open_flag = True
         self.close_flag = False
         self.end_flag = False
@@ -79,6 +85,7 @@ class data_generator:
             for book, num in books.items():
                 temp_commands.append(f"{book} {num}\n")
                 self.library.add_book(book, num)
+                self.books.add(book)
         random.shuffle(temp_commands)
         self.commands.extend(temp_commands)
         self.init_command_list = [str(len(temp_commands)) + '\n'] + temp_commands
@@ -91,7 +98,7 @@ class data_generator:
             self.library.persons[id] = Person(id)
 
     def generate_basic_commands(self):
-        global command_num, query_prob, borrow_prob, order_prob
+        global command_num, query_prob, borrow_prob, order_prob, donate_prob
         dates = generate_basic_date()
         t_command_num = random.randint(max(command_num // 2, 1), command_num)
         tmp_commands = []
@@ -101,26 +108,40 @@ class data_generator:
                 tmp_commands.append(self.generate_query())
             elif prob < query_prob + borrow_prob:
                 tmp_commands.append(self.generate_borrow())
-            else:
+            elif prob < query_prob + borrow_prob + order_prob:
                 tmp_commands.append(self.generate_order())
+            elif prob < query_prob + borrow_prob + order_prob + donate_prob:
+                tmp_commands.append(self.generate_donate())
         tmp_commands = split_list_randomly(tmp_commands, len(dates))
         for date in dates:
             self.date2commands[date] = tmp_commands.pop(0)
 
     def generate_query(self) -> str:
-        person_id = random.choice(list(self.library.persons.keys()))
-        book = random.choice(list(self.library.bs.keys()))
+        person_id = random.choice(list(self.books))
+        book = random.choice(list(self.books))
         return f"{person_id} queried {book}\n"
 
     def generate_borrow(self) -> str:
         person_id = random.choice(list(self.library.persons.keys()))
-        book = random.choice(list(self.library.bs.keys()))
+        book = random.choice(list(self.books))
         return f"{person_id} borrowed {book}\n"
 
     def generate_order(self) -> str:
         person_id = random.choice(list(self.library.persons.keys()))
-        book = random.choice(list(self.library.bs.keys()))
+        book = random.choice(list(self.books))
         return f"{person_id} ordered {book}\n"
+
+    def generate_donate(self) -> str:
+        person_id = random.choice(list(self.library.persons.keys()))
+        while True:
+            type = random.choice(['AU', 'BU', 'CU'])
+            number = random.randint(0, 9999)
+            book = type.rstrip("U") + f"-{number: 04}"
+            if book in self.books:
+                continue
+            else:
+                self.books.add(book)
+                return f"{person_id} donated {type}-{number:04}\n"
 
     def generate_pick(self, person_id, book_id):
         return f"{person_id} picked {book_id}\n"
@@ -128,40 +149,75 @@ class data_generator:
     def generate_return(self, person_id, book_id):
         return f"{person_id} returned {book_id}\n"
 
+    def generate_renew(self, person_id, book_id):
+        return f"{person_id} renewed {book_id}\n"
+
     def add_command(self, output=""):
         """
         将输出结果导入数据生成器
         :param output: 生成的字符串
         :return:
         """
-        global e_date, pick_1_prob, pick_2_prob, return_prob
+        global e_date, pick_1_prob, pick_2_prob, return_prob, renew_prob
         if 'accept' in output:
             date_str = output.split(" ")[0].strip("\n")
-            book_id = output.split(" ")[-1].strip("\n")
+            book_id = output.split(" ")[4].strip("\n")
             person_id = output.split(" ")[2].strip("\n")
             date = datetime.strptime(date_str, "[%Y-%m-%d]")
             date_str = str(date.date())
             end_date = datetime.strptime(e_date, "%Y-%m-%d")
-            if 'borrowed' in output and "picked" in output:
-                if random.random() < return_prob:
+            if 'borrowed' in output or "picked" in output:  # 生成还书命令
+                prob1 = random.random()
+                prob2 = random.random()
+                if prob1 < return_prob and prob2 > renew_prob:  # 仅还书
                     delta = end_date - date
                     next_date_str = str((date + timedelta(days=random.randint(0, delta.days))).date())
                     command = self.generate_return(person_id, book_id)
-                    if next_date_str == date_str:
-                        insert_place = random.randint(self.index, len(self.date2commands[date_str]))
-                        self.date2commands[date_str].insert(insert_place, command)
-                    elif next_date_str in self.date2commands:
-                        insert_place = random.randint(0, len(self.date2commands[next_date_str]))
-                        self.date2commands[next_date_str].insert(insert_place, command)
+                    self.randomly_insert(command, date_str, next_date_str)
+                elif prob1 > return_prob and prob2 < renew_prob:  # 仅续借:
+                    if random.random() < 0.5:
+                        delta = end_date - date
+                        next_date_str = str((date + timedelta(days=random.randint(0, delta.days))).date())
+                        command = self.generate_renew(person_id, book_id)
+                        self.randomly_insert(command, date_str, next_date_str)
                     else:
-                        self.date2commands[next_date_str] = [command]
-                        self.date2commands = dict(sorted(self.date2commands.items()))
-            if 'ordered' in output:
+                        if 'BU' in book_id:
+                            limit_days = 7
+                        elif 'B' in book_id:
+                            limit_days = 30
+                        elif 'CU' in book_id:
+                            limit_days = 60
+                        else:
+                            limit_days = 14
+                        limit_date = min(date + timedelta(days=limit_days), end_date)
+                        next_date_str = str(max((limit_date - timedelta(days=random.randint(0, 5))), date).date())
+                        command = self.generate_renew(person_id, book_id)
+                        self.randomly_insert(command, date_str, next_date_str)
+                elif prob1 < return_prob and prob2 < renew_prob:  # 还书并续借
+                    if 'BU' in book_id:
+                        limit_days = 7
+                    elif 'B' in book_id:
+                        limit_days = 30
+                    elif 'CU' in book_id:
+                        limit_days = 60
+                    else:
+                        limit_days = 14
+                    limit_date = min(date + timedelta(days=limit_days), end_date)
+                    next_date_str = str(max((limit_date - timedelta(days=random.randint(0, 5))), date).date())
+                    command = self.generate_renew(person_id, book_id)
+                    begin_date_str, begin_index = self.randomly_insert(command, date_str, next_date_str)  # 生成续借
+                    delta = end_date - begin_date_str
+                    next_date_str = str((begin_date_str + timedelta(days=random.randint(0, delta.days))).date())
+                    command = self.generate_return(person_id, book_id)
+                    self.randomly_insert(command, begin_date_str, next_date_str, begin_index)  # 生成还书
+
+            if 'ordered' in output:  # 生成取书命令
                 prob = random.random()
                 if prob < pick_2_prob:  # 生成两条pick
                     command = self.generate_pick(person_id, book_id)
                     for i in range(2):
-                        next_date_str = str((date + timedelta(days=random.randint(0, min(12, (end_date - date).days)))).date())
+                        next_date_str = str(
+                            (date + timedelta(days=random.randint(0, min(12, (end_date - date).days)))).date())
                         if next_date_str == date_str:
                             insert_place = random.randint(self.index, len(self.date2commands[date_str]))
                             self.date2commands[date_str].insert(insert_place, command)
@@ -173,7 +229,8 @@ class data_generator:
                             self.date2commands = dict(sorted(self.date2commands.items()))
                 elif prob < pick_1_prob + pick_2_prob:  # 生成一条pick
                     command = self.generate_pick(person_id, book_id)
-                    next_date_str = str((date + timedelta(days=random.randint(0, min(12, (end_date - date).days)))).date())
+                    next_date_str = str(
+                        (date + timedelta(days=random.randint(0, min(12, (end_date - date).days)))).date())
                     if next_date_str == date_str:
                         insert_place = random.randint(self.index, len(self.date2commands[date_str]))
                         self.date2commands[date_str].insert(insert_place, command)
@@ -183,6 +240,9 @@ class data_generator:
                     else:
                         self.date2commands[next_date_str] = [command]
                         self.date2commands = dict(sorted(self.date2commands.items()))
+        elif 'U' in output and "from bro to bs" in output:
+            book_id = output.split(" ")[2].strip("\n")
+            self.update_U_book(book_id)
 
     def get_next_command(self) -> str:
         """
@@ -213,6 +273,27 @@ class data_generator:
                 self.close_flag = True
         self.commands.append(command)
         return command
+
+    def randomly_insert(self, command, date_str, next_date_str, begin_index=-1):
+        if begin_index == -1:
+            begin_index = self.index
+        if next_date_str == date_str:
+            insert_place = random.randint(begin_index, len(self.date2commands[date_str]))
+            self.date2commands[date_str].insert(insert_place, command)
+        elif next_date_str in self.date2commands:
+            insert_place = random.randint(0, len(self.date2commands[next_date_str]))
+            self.date2commands[next_date_str].insert(insert_place, command)
+        else:
+            self.date2commands[next_date_str] = [command]
+            self.date2commands = dict(sorted(self.date2commands.items()))
+            insert_place = 0
+        return next_date_str, insert_place
+
+    def update_U_book(self, book_id):
+        for key, value in self.date2commands:
+            for i, command in enumerate(value):  # 使用enumerate获取列表中的索引和元素
+                if book_id in command:
+                    value[i] = command.replace("U", '')
 
 
 if __name__ == '__main__':
